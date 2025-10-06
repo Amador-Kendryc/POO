adb8f49473dcc230c06ddf23c34ade9120781bb1
Amador_Kendryc <kendryc_3141240247@utd.edu.mx>
2025-10-05 22:38:15 -0600

    Add files via upload

---
+++ P1/1_clases_objetos/practica_Coche.py
@@ -0,0 +1,28 @@
+import os
+os.system("cls")
+
+
+
+class Coche:
+    marca=""
+    color=""
+    modelo=""
+    velocidad=0
+    caballaje=0
+    plazas=0
+
+
+   
+    def acelerar(self):   
+        pass
+
+    def frenar(self):
+        pass
+
+coche1=Coche()
+coche1.marca="vw"
+coche1.color="blanco"
+coche1.modelo="2022"
+coche1.velocidad=180
+coche1.caballaje=150
+coche1.plazas=6
